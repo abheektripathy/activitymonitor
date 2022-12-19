6419d51e0e84fa3b580cfc1d4dc6041c48a794a3
Abheek Tripathy <abheek.tripathy@gmail.com>
2022-12-20 02:01:13 +0530

    ui

--- components/ApplicationContainer.tsx
+++ components/ApplicationContainer.tsx
@@ -4,7 +4,48 @@ export const ApplicationContainer = ({ //@ts-ignore
     children }) => {
   return (
     <AppShell
-      
+      styles={{
+        main: {
+            backgroundColor: 'black',
+            width: '100vw',
+            height: '100vh',
+            paddingLeft: '0px',
+
+
+        }
+      }}
+      fixed
+      footer={<Footer height={60} p= 'md'>
+        <Group position="apart" spacing='xl'>
+            <Text size='sm'>
+                <span style={{fontWeight: "bolder"}}>
+                  🚀 List Time 
+                </span> .25 hrs 
+            </Text>
+            <Text size='sm'>
+                <span style={{fontWeight: "bolder"}}>
+                   🎉 End Time 
+                </span> .25 hrs 
+            </Text>             
+        </Group>
+
+      </Footer>}
+      header={<Header height={60} p='md'>
+        <Group position="apart" spacing='xl'>
+            <Text size='xl'>
+            <span style={
+            {
+                fontWeight: "bolder",
+                fontSize: "1.2rem",
+            }
+        }>Alpaca</span>    Activity.
+            </Text>
+        
+        <span style={{
+            fontSize: "1.3rem",
+        }}>🦙</span>
+        </Group>
+      </Header>}
     >
       {children}
     </AppShell>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from 'next/app'
 import '../styles/globals.css'
 
 import { MantineProvider } from '@mantine/core'
+import { ApplicationContainer } from '../components/ApplicationContainer'
 
 export default function App({ //@ts-ignore
   Component, pageProps }) {
@@ -10,6 +11,8 @@ export default function App({ //@ts-ignore
     withGlobalStyles 
     theme={{ colorScheme: 'dark' }}  
   >
+    <ApplicationContainer>
     <Component {...pageProps} />
+    </ApplicationContainer>
   </MantineProvider>
 }
