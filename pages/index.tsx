05e5386aec6093db726652aef182f2ac9ff922aa
Abheek Tripathy <abheek.tripathy@gmail.com>
2022-12-20 12:41:12 +0530

    ui-fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 import { StatsGrid } from '../components/xyz'
 import { HeaderMiddle } from '../components/header2'
 import { TableReviews } from '../components/table'
-import { Group, ActionIcon, Box, Text, Title, UnstyledButton, useMantineTheme  } from '@mantine/core'
+import { Group, ActionIcon, Box, Text, Title, UnstyledButton, useMantineTheme, Center  } from '@mantine/core'
 import { IconPlayerPlay, IconSettings, IconPlaylistAdd, IconPlus } from "@tabler/icons";
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,15 +16,24 @@ export default function Home() {
 
       <Group //@ts-ignore
        direction ="column" mt = "-20px">
-        <Group style={{backgroundColor: '#0c0c0c', height: '100vh', width:'400px', 
+        <Group style={{backgroundColor: '#0c0c0c', height: '92vh', width:'400px', 
         // marginTop:'25px', borderRadius: "10px"
       }} mx="auto" //@ts-ignore
-        direction = "column" >
+        direction = "row" >
           <Group position='apart' style={{ width: "100%"}}>
-            <ActionIcon color="gray" variant="transparent" size="lg" style={{marginTop: "-600px", marginLeft: "15px"}}><IconSettings></IconSettings></ActionIcon>
-            <ActionIcon color="gray" variant="transparent" size="lg" style={{marginTop: "-600px", marginRight: "20px"}}><IconPlaylistAdd></IconPlaylistAdd></ActionIcon>
+            <ActionIcon color="gray" variant="transparent" size="lg" style={{marginTop: "-300px", marginLeft: "15px"}}><IconSettings></IconSettings></ActionIcon>
+            <ActionIcon color="gray" variant="transparent" size="lg" style={{marginTop: "-300px", marginRight: "20px"}}><IconPlaylistAdd></IconPlaylistAdd></ActionIcon>
+            
           </Group>
+          <Group spacing="none" mt = 'sm' position='center'  mx = 'auto' direction= 'column' height = '176px' style={{marginTop: "-800px"}} >
+            <Title order={1} style={{fontWeight: 'bolder', fontSize: '4rem'}}>25:00</Title>
+            
+          </Group>
+          
+          
+         
         </Group>
+      
       </Group>
 
 
