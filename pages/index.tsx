2c08e0c1aa6ddc28c9f730e5642fa7468016feae
Abheek Tripathy <abheek.tripathy@gmail.com>
2022-12-20 18:49:16 +0530

    ui-fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,42 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-import { StatsGrid } from '../components/xyz'
-import { HeaderMiddle } from '../components/header2'
-import { TableReviews } from '../components/table'
-import { Group, ActionIcon, Box, Text, Title, UnstyledButton, useMantineTheme, Center  } from '@mantine/core'
-import { IconPlayerPlay, IconSettings, IconPlaylistAdd, IconPlus } from "@tabler/icons";
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
+
+import {
+  Group,
+  ActionIcon,
+  Box,
+  Text,
+  Title,
+  UnstyledButton,
+  useMantineTheme,
+  Center,
+  Flex,
+} from "@mantine/core";
+import {
+  IconPlayerPlay,
+  IconSettings,
+  IconPlaylistAdd,
+  IconPlus,
+} from "@tabler/icons";
+import { FeaturesCard } from "../components/card";
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const theme = useMantineTheme();
   return (
-    
-
-      <Group //@ts-ignore
-       direction ="column" mt = "-20px">
-        <Group style={{backgroundColor: '#0c0c0c', height: '92vh', width:'400px', 
-        // marginTop:'25px', borderRadius: "10px"
-      }} mx="auto" //@ts-ignore
-        direction = "row" >
-          <Group position='apart' style={{ width: "100%"}}>
-            <ActionIcon color="gray" variant="transparent" size="lg" style={{marginTop: "-300px", marginLeft: "15px"}}><IconSettings></IconSettings></ActionIcon>
-            <ActionIcon color="gray" variant="transparent" size="lg" style={{marginTop: "-300px", marginRight: "20px"}}><IconPlaylistAdd></IconPlaylistAdd></ActionIcon>
-            
-          </Group>
-          <Group //@ts-ignore
-          spacing="none" mt = 'sm' position='center'  mx = 'auto' direction= 'column' height = '176px' style={{marginTop: "-800px"}} >
-            <Title order={1} style={{fontWeight: 'bolder', fontSize: '4rem'}}>25:00</Title>
-            
-          </Group>
-          
-          
-         
-        </Group>
-      
-      </Group>
-
-
-      
-      
-           
-    
-  )
+    <>
+     
+      <Flex
+      direction={{ base: 'column', sm: 'row' }}
+      gap={{ base: 'lg', sm: 'lg' }}
+      justify={{ sm: 'center' }}
+    >
+        <FeaturesCard></FeaturesCard>
+        <FeaturesCard></FeaturesCard>
+        <FeaturesCard></FeaturesCard>
+      </Flex>
+    </>
+  );
 }
